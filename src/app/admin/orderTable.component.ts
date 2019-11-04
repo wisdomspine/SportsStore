485145875a3de1a1797ccdb872de1f98ec8f1f27
wisdomspine <sabopriest@gmail.com>
2019-11-04 16:36:41 +0100

    product and order tables' component

---
+++ src/app/admin/orderTable.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: `<div class="bg-warning p-2 text-white">
+                <h3>Order Table Placeholder</h3>
+            </div>
+            `
+})
+
+export class OrderTableComponent {
+    constructor() {
+
+    }
+}
---
+++ src/app/admin/productEditor.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+        <div class="bg-warning p-2 text-white">
+            <h3>Product Editor Placeholder</h3>
+        </div>
+    `
+})
+export class ProductEditorComponent {
+    constructor() {
+
+    }
+}
