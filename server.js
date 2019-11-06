f6ac5e3919d297ac7c94e472b22b0a5e8be9a2c7
wisdomspine <sabopriest@gmail.com>
2019-11-06 01:07:17 +0100

    heroku repo created and app deployed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import history from "connect-history-api-fallback";
 import jsonServer from "json-server";
 import bodyParser from "body-parser";
 import auth from "./authMiddleware";
+import process from "process";
 
 const router= jsonServer.router("data.json");
 
@@ -16,14 +17,15 @@ if(enableHttps){
 }
 
 const app= express();
+const PORT=process.env.PORT || 9000;
 
 app.use(bodyParser.json());
 app.use(auth);
 app.use("/api", router);
 app.use(history());
-app.use("/", express.static("./dist/SportsStore"));
+app.use("/", express.static("./dist/SportStore"));
 
-app.listen(80, ()=> console.log("Server running on port 80"));
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
 
 if(enableHttps){
     https.createServer(sslOptions, app).listen(443, ()=>console.log("HTTPS Server running on port 443"));
